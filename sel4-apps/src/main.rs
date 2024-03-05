d8852250cf775eafcf00ec22c0fdf75febdb6763
Avery Blanchard <ab1036@duke.edu>
2024-03-05 11:28:02 -0500

    Update arch
    Signed-off-by: Avery Blanchard <ab1036@duke.edu>

--- sel4-apps/src/main.rs
+++ sel4-apps/src/main.rs
@@ -1,19 +1,24 @@
 #![no_std]
 
 
-pub fn sys_send_null(sys: c_int, src: word, info_arg: msg_info) {
+pub fn sys_send_null(sys: c_int, src: seL4_Word, info_arg: seL4_MessageInfo) {
     unsafe {
-        asm!("ecall",
-            in("a7") sys_id_to_word(sys),
-            in("a0") src,
-            in("a1") ,
+        asm!(
+            "mov r14, rsp",
+            "syscall",
+            "mov rsp, r14",
+            in("rdx") sys_id_to_word(sys),
+            in("rdi") src,
+            in("rsi") info_arg.into_word(),
+            lateout("rcx") _,
+            lateout("r11") _,
+            lateout("r14") _,
         );
     }
 }
 
-pub fn seL4_Signal(&mut self, dest: ptr) {
-      let msg_info = sys::new(0, 0, 0, 0);
-
+pub fn seL4_Signal(&mut self, dest: seL4_CPtr) {
+      let msg_info = seL4_MessageInfo::new(0, 0, 0, 0);
       sys_send_null(syscall_id::Send, dest, msg_info)
 }
 
