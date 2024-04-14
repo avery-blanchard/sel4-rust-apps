e2c6c640322926c9e5fd03ee283955636db10803
Avery Blanchard <ab1036@duke.edu>
2024-04-14 07:32:45 -0400

    Update
    Signed-off-by: Avery Blanchard <ab1036@duke.edu>

--- sel4-apps/src/main.rs
+++ sel4-apps/src/main.rs
@@ -1,92 +1,47 @@
 #![no_std]
+#![no_main]
+#![feature(naked_functions)]
+#![feature(decl_macro)]
 
+use core::panic::PanicInfo;
+use core::arch::asm;
 
-pub type c_int = i32; // hardcoded, this might depend
-
-pub type seL4_Word = int<16>; // again, hardcoded. 
-
-pub struct PhantomData<T>
-where
-    T: ?Sized;
-
-pub macro asm("assembly template", $(operands,)* $(options($(option),*))?) {
-    /* compiler built-in */
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
 
-#[repr(transparent)]
+pub type CInt = i32;
 
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct Bitfield<T, U> {
-    inner: T,
-    _phantom: PhantomData<U>,
-}
+pub type SeL4Word = i32;
 
-impl<T, U> Bitfield<T, U> {
-    pub fn new(inner: T) -> Self {
-        Self {
-            inner,
-            _phantom: PhantomData,
-        }
-    }
 
-    pub fn into_inner(self) -> T {
-        self.inner
-    }
-
-    pub fn inner(&self) -> &T {
-        &self.inner
-    }
-
-    pub fn inner_mut(&mut self) -> &mut T {
-        &mut self.inner
-    }
-}
-
-pub(crate) type SeL4Bitfield<T, const N: usize> = Bitfield<[T; N], T>;
-
-impl seL4_MessageInfo {
-    pub(crate) fn from_word(word: seL4_Word) -> Self {
-        Self(SeL4Bitfield::new([word]))
-    }
-
-    pub(crate) fn into_word(self) -> seL4_Word {
-        self.0.into_inner()[0]
-    }
-}
-
-pub fn sys_id_to_word(sys_id: c_int) -> seL4_Word {
-    sys_id as seL4_Word
-}
-
-pub fn sys_recv(
-    sys: c_int,
-    src: seL4_Word,
-    out_mr0: &mut seL4_Word,
-    out_mr1: &mut seL4_Word,
-    out_mr2: &mut seL4_Word,
-    out_mr3: &mut seL4_Word,
-    reply: seL4_Word,
-) -> (seL4_MessageInfo, seL4_Word) {
-    let out_info: seL4_Word;
-    let out_badge: seL4_Word;
+pub fn main() {
     unsafe {
-        asm!(
+	let sys = 3; // system call numbers are generated when the kernel is built, hardcoded
+        let out_badge: SeL4Word;
+        let out_info: SeL4Word;
+        let mut mr0 = 0;
+        let mut mr1 = 0;
+        let mut mr2 = 0;
+        let mut mr3 = 0;
+
+        asm!(	
             "mov r14, rsp",
             "syscall",
             "mov rsp, r14",
-            in("rdx") sys_id_to_word(sys),
-            inout("rdi") src => out_badge,
+            in("rdx") sys,
+            inout("rdi") 0 => out_badge,
             out("rsi") out_info,
-            out("r10") *out_mr0,
-            out("r8") *out_mr1,
-            out("r9") *out_mr2,
-            out("r15") *out_mr3,
-            in("r12") reply,
+            out("r10") mr0,
+            out("r8") mr1,
+            out("r9") mr2,
+            out("r15") mr3,
+            in("r12") 0,
             lateout("rcx") _,
             lateout("r11") _,
             lateout("r14") _,
-        );
+            );
     }
-    (seL4_Message_info::from_word(out_info), out_badge)
-}
 
+}
